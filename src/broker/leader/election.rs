4e52edbb8fd96060fda94437681c811385867305
AndreVale69 <sforzini31@gmail.com>
2025-02-23 01:35:20 +0100

    docs(broker/election): add description and examples
    Refs: #36

--- src/broker/leader/election.rs
+++ src/broker/leader/election.rs
@@ -1,3 +1,26 @@
+//! Module for leader election.
+//!
+//! The `LeaderElection` struct manages the leader election process for a broker.
+//! This struct provides methods to create a new leader election instance,
+//! start the election process, and request votes from peers.
+//!
+//! # Example
+//! The following example demonstrates how to create a new leader election instance
+//! and start the election process.
+//! ```
+//! use pilgrimage::broker::leader::election::LeaderElection;
+//! use pilgrimage::broker::leader::state::BrokerState;
+//! use std::collections::HashMap;
+//!
+//! let peers = HashMap::new();
+//! let election = LeaderElection::new("broker1", peers);
+//! let elected = election.start_election();
+//!
+//! // Check if the broker was elected as leader
+//! assert!(elected);
+//! assert_eq!(*election.state.lock().unwrap(), BrokerState::Leader);
+//! ```
+
 use super::heartbeat::Heartbeat;
 use super::state::{BrokerState, Term};
 use std::collections::HashMap;
@@ -5,13 +28,24 @@ use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 
+/// The `LeaderElection` struct manages the leader election process for a broker.
+///
+/// This struct provides methods to create a new leader election instance,
+/// start the election process, and request votes from peers.
 #[derive(Clone)]
 pub struct LeaderElection {
+    /// The ID of the broker.
     pub broker_id: String,
+    /// The current state of the broker.
+    /// This is shared between the leader election and heartbeat threads.
     pub state: Arc<Mutex<BrokerState>>,
+    /// The current term of the broker.
     pub term: Arc<Mutex<Term>>,
+    /// A hashmap of peer broker IDs and their addresses.
     pub peers: Arc<Mutex<HashMap<String, String>>>,
+    /// The last time a heartbeat was sent.
     pub last_heartbeat: Arc<Mutex<Instant>>,
+    /// The duration of the election timeout.
     pub election_timeout: Duration,
 }
 
@@ -49,6 +83,14 @@ impl LeaderElection {
 
     /// Starts the leader election process.
     ///
+    /// This method transitions the broker to a candidate state, increments the current term,
+    /// and requests votes from peers.
+    /// If the broker receives a majority of votes, it transitions
+    /// to a leader state and starts the heartbeat thread.
+    ///
+    /// # Returns
+    /// `true` if the broker was elected as leader, `false` otherwise.
+    ///
     /// # Examples
     ///
     /// ```
@@ -85,6 +127,12 @@ impl LeaderElection {
         }
     }
 
+    /// Requests votes from peers.
+    ///
+    /// This method sends a vote request to each peer and waits for a response.
+    ///
+    /// # Returns
+    /// The number of votes received.
     fn request_votes(&self) -> usize {
         let votes = 1;
         let peers = self.peers.lock().unwrap();
@@ -96,6 +144,15 @@ impl LeaderElection {
 mod tests {
     use super::*;
 
+    /// Tests the leader election process.
+    ///
+    /// # Purpose
+    /// This test verifies that a broker can be elected as leader.
+    ///
+    /// # Steps
+    /// 1. Create a new leader election instance.
+    /// 2. Start the election process.
+    /// 3. Verify that the broker was elected as leader.
     #[test]
     fn test_leader_election() {
         let mut peers = HashMap::new();
