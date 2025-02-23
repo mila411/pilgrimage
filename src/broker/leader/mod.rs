c1eafd4e76b25377641489726652acddadf488bf
AndreVale69 <sforzini31@gmail.com>
2025-02-23 01:50:43 +0100

    docs(broker/leader): add description
    Refs: #36

--- src/broker/leader/mod.rs
+++ src/broker/leader/mod.rs
@@ -1,3 +1,7 @@
+//! Module for leader election and broker leader state.
+//!
+//! This module contains the implementation of leader election and broker leader state.
+
 pub mod election;
 pub mod heartbeat;
 pub mod state;
